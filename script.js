86a47077888c6ef310124ec9febcde506b425bda
ethanfronton <146748745+ethanfronton@users.noreply.github.com>
2024-12-23 15:55:52 +0100

    [ADD] --> calculatrice

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function calculatrice(nombre1, nombre2, operateur) {
+    switch (operateur) {
+        case '+':
+            return nombre1 + nombre2;
+        case '-':
+            return nombre1 - nombre2;
+        case '*':
+            return nombre1 * nombre2;
+        case '/':
+            if (nombre2 !== 0) {
+                return nombre1 / nombre2;
+            } else {
+                return 'Erreur: Division par zéro';
+            }
+        default:
+            return 'Erreur: Opérateur non valide';
+    }
+}
+
 function calculer() {
     const nombre1 = parseFloat(document.getElementById('nombre1').value);
     const operateur = document.getElementById('operateur').value;
