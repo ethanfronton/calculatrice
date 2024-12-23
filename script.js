cabf46ad6e93a30c620d798cb9b98431a5062464
ethanfronton <146748745+ethanfronton@users.noreply.github.com>
2024-12-23 16:01:03 +0100

    [ADD] --> change operateur

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function calculatrice(nombre1, nombre2, operateur) {
 
 function calculer() {
     const nombre1 = parseFloat(document.getElementById('nombre1').value);
-    const operateur = document.getElementById('operateur').value;
+    const operateur = document.getElementById('operation').value;
     const nombre2 = parseFloat(document.getElementById('nombre2').value);
     const resultat = calculatrice(nombre1, nombre2, operateur);
     document.getElementById('resultat').value = resultat;
